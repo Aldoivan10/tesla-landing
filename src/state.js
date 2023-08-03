7940a9594a642a0235e66605cd9899a93236e1b4
aldoivan10 <aldoivan_palma@hotmail.com>
2023-08-03 00:02:51 -0600

    Prettier: Added esLint format

--- src/state.js
+++ src/state.js
@@ -2,42 +2,29 @@ let firstTimeShown = true
 let currentTab = ''
 let timeOut
 
-export const hoverButton = (newTab) =>
-{
-    if(newTab !== currentTab)
-    {
-        if(currentTab)
-        {
-            const content = document.getElementById(currentTab)
-
-            content.classList.remove('flex')
-            content.classList.add('hidden')
-        }
-
-        if(newTab)
-        {
-            const content = document.getElementById(newTab)
-
-            if(firstTimeShown)
-            {
-                timeOut = setTimeout(() => 
-                {  
-                    content.classList.remove('hidden')
-                    content.classList.add('flex')
-                    timeOut = null
-                }, 450)
-            }
-            else
-            {
-                if(timeOut) clearTimeout(timeOut)
-                content.classList.remove('hidden')
-                content.classList.add('flex')
-            }
-
-            firstTimeShown = false
-        }
-        else firstTimeShown = true
-
-        currentTab = newTab
+export const hoverButton = (newTab) => {
+  if (newTab !== currentTab) {
+    if (currentTab) {
+      const content = document.getElementById(currentTab)
+      content.classList.replace('flex', 'hidden')
     }
+
+    if (newTab) {
+      const content = document.getElementById(newTab)
+
+      if (firstTimeShown) {
+        timeOut = setTimeout(() => {
+          content.classList.replace('hidden', 'flex')
+          timeOut = null
+        }, 450)
+      } else {
+        if (timeOut) clearTimeout(timeOut)
+        content.classList.replace('hidden', 'flex')
+      }
+
+      firstTimeShown = false
+    } else firstTimeShown = true
+
+    currentTab = newTab
+  }
 }
