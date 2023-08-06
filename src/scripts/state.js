56da83a191642514c04a627fbfaf335c1deec623
aldoivan10 <aldoivan_palma@hotmail.com>
2023-08-05 23:02:15 -0600

    Refactoring code

--- src/scripts/state.js
+++ src/scripts/state.js
@@ -4,33 +4,43 @@ let timeOut
 
 export const hoverButton = (newTab) => {
   if (newTab !== currentTab) {
-    if (currentTab) {
-      const content = document.getElementById(currentTab)
-      content.classList.replace('opacity-100', 'opacity-0')
-      timeOut = setTimeout(() => {
-        content.classList.replace('flex', 'hidden')
-        timeOut = null
-      }, 300)
-    }
+    if (currentTab) hideTab(currentTab)
 
     if (newTab) {
-      const content = document.getElementById(newTab)
-
-      if (firstTimeShown) {
-        timeOut = setTimeout(() => {
-          content.classList.replace('hidden', 'flex')
-          content.classList.replace('opacity-0', 'opacity-100')
-          timeOut = null
-        }, 450)
-      } else {
-        if (timeOut) clearTimeout(timeOut)
-        content.classList.replace('hidden', 'flex')
-        content.classList.replace('opacity-0', 'opacity-100')
-      }
-
+      showTab(newTab, firstTimeShown)
       firstTimeShown = false
     } else firstTimeShown = true
 
     currentTab = newTab
   }
+}
+
+function hideTab (tab) {
+  const content = document.getElementById(tab)
+  content.classList.replace('opacity-100', 'opacity-0')
+  timeOut = setTimeout(() => {
+    content.classList.replace('flex', 'hidden')
+    timeOut = null
+  }, 300)
+}
+
+function showTab (tab, firstTimeShown) {
+  const content = document.getElementById(tab)
+  const divs = content.querySelectorAll('.d-content')
+  const leftDiv = divs[0]
+  const rightDiv = divs[1]
+
+  timeOut = setTimeout(() => {
+    rightDiv.classList.replace('hidden', 'flex')
+    rightDiv.classList.replace('opacity-0', 'opacity-100')
+    timeOut = null
+  }, 300)
+
+  /* if (firstTimeShown) {
+
+  } else {
+    if (timeOut) clearTimeout(timeOut)
+    content.classList.replace('hidden', 'flex')
+    content.classList.replace('opacity-0', 'opacity-100')
+  } */
 }
